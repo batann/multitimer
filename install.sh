601fb2991adb2ea342b73715e68857bfb69e7517
batann <122584289+batann@users.noreply.github.com>
2024-10-14 21:46:47 +1100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo cp $HOME/multitimer/multitimer /usr/bin/lc-multitimer
+sudo cp $HOME/multitimer/multitimer $HOME/dot/bin/lc-multitimer
+sudo trash $HOME/multitimer
+clear
+echo -e "\033[36m Installed lc-multitimer\033[0m"
